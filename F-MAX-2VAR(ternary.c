2eafc381b23ddee5a3f65a81f8d87c42cf7783f2
Bhargav <128204030+Bhargav5427@users.noreply.github.com>
2023-08-07 14:34:27 +0530

    Add files via upload

---
+++ F-MAX-2VAR(ternary.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+main()
+{
+	int a,b;
+	
+		printf("enter the value of A :- ");
+		scanf("%d",&a);
+		
+		printf("enter the value of B :- ");
+		scanf("%d",&b);
+
+
+		
+	(a>b)? printf("A is max"):printf("b is max");
+}
